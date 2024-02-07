d2a8538232aa36d8cd917f1f96c66eb6ef48a541
EivindHY <eivind.hyttnes@hotmail.com>
2024-02-07 10:44:08 +0100

    fuck this garbage

---
+++ main.py
@@ -0,0 +1,38 @@
+import cv2
+from deepface import DeepFace
+
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+face_match = False
+
+reference_img = cv2.imread("image.jpg")
+
+def check_face(frame):
+    try:
+        result = DeepFace.verify(frame, reference_img.copy())
+        return result["verified"]
+    except Exception as e:
+        print("Error during face verification:", e)
+        return False
+
+while True: 
+    ret, frame = cap.read()
+
+    if ret:
+        face_match = check_face(frame)
+
+        if face_match:
+            cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        else:
+            cv2.putText(frame, "No match", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+
+        cv2.imshow("video", frame)
+
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
